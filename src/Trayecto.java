488236ce195e04cc9648fed303987550248f44d5
JeremiasMVL <71725020+JeremiasMVL@users.noreply.github.com>
2021-07-27 22:59:22 -0300

    Primer commit clase

---
+++ src/Trayecto.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//Clase?
+
+public class Trayecto {
+
+	private List<EstacionDeTransbordoMultimodal> estaciones; //?
+	private List<Ruta> tramos; 
+	//Solo una de las listas?
+	
+	public Trayecto() {
+		this.estaciones = new ArrayList<EstacionDeTransbordoMultimodal>();
+		this.tramos = new ArrayList<Ruta>();
+	}
+	
+	//
+}	
+	
+	
