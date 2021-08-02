c42f6987cb97f66edfa7f3308db3e0a98c321420
Martin Ibañez <mibanezweller@gmail.com>
2021-08-02 01:25:52 -0300

    Terminadas la interfaz de Mantenimiento de Estaciones

--- src/interfaces/registrarMantenimientoEstacion/InterfazRegistrarMantenimientoEstacion.java
+++ src/interfaces/registrarMantenimientoEstacion/InterfazRegistrarMantenimientoEstacion.java
@@ -4,8 +4,6 @@ import interfaces.InterfazFrame;
 import interfaces.InterfazPrincipal;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
 import java.awt.*;
 
 public class InterfazRegistrarMantenimientoEstacion {
@@ -38,16 +36,45 @@ public class InterfazRegistrarMantenimientoEstacion {
         cons0.insets = new Insets(55,0,20,0);
         panelRegistrarMantenimiento.add(nombreMenu, cons0);
 
+        GridBagConstraints cons1 = new GridBagConstraints();
+        String[] data = {"Estacion1", "Estacion2", "Estacion3", "Estacion4", "Estacion5", "Estacion6", "Estacion7", "Estacion8", "Estacion9", "Estacion10"};
+        JList<String> campoLista = new JList<>(data);
+        cons1.gridwidth = 2;
+        cons1.gridx = 0;
+        cons1.gridy = 1;
+        cons1.fill = GridBagConstraints.BOTH;
+        cons1.insets = new Insets(10,0,40,0);
+        panelRegistrarMantenimiento.add(campoLista, cons1);
 
+        GridBagConstraints cons2 = new GridBagConstraints();
+        JLabel estadoEstacion = new JLabel("Estado de la Estacion: ");
+        estadoEstacion.setFont(new Font("Dialog", Font.BOLD, 15));
+        cons2.gridwidth = 2;
+        cons2.gridx = 0;
+        cons2.gridy = 2;
+        cons2.fill = GridBagConstraints.HORIZONTAL;
+        cons2.insets = new Insets(10,0,20,0);
+        panelRegistrarMantenimiento.add(estadoEstacion, cons2);
+
+        GridBagConstraints cons3 = new GridBagConstraints();
+        JComboBox<String> campoEstado = new JComboBox<>();
+        cons3.gridwidth = 2;
+        cons3.gridx = 0;
+        cons3.gridy = 3;
+        cons3.fill = GridBagConstraints.HORIZONTAL;
+        cons3.insets = new Insets(5, 5 ,20 ,5);
+        campoEstado.addItem("Operativa");
+        campoEstado.addItem("En Mantenimiento");
+        panelRegistrarMantenimiento.add(campoEstado,cons3);
 
         GridBagConstraints cons12 = new GridBagConstraints();
         JButton botonAtras = new JButton("Atras");
         cons12.gridwidth = 2;
         cons12.gridx = 0;
-        cons12.gridy = 7;
+        cons12.gridy = 4;
         cons12.fill = GridBagConstraints.HORIZONTAL;
         cons12.anchor = GridBagConstraints.SOUTH;
-        cons12.insets = new Insets(10,0,60,0);
+        cons12.insets = new Insets(40,0,60,0);
         panelRegistrarMantenimiento.add(botonAtras,cons12);
 
 
