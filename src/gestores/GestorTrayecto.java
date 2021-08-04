ebd8943b5a5d1c81f226dcab0b0ddf8e7d29c1ca
JeremiasMVL <71725020+JeremiasMVL@users.noreply.github.com>
2021-08-03 23:12:37 -0300

    update limpieza comentarios

--- src/gestores/GestorTrayecto.java
+++ src/gestores/GestorTrayecto.java
@@ -1,7 +1,6 @@
 package gestores;
 
 import java.sql.SQLException;
-//import java.util.ArrayList;
 import java.util.List;
 
 import dao.Trayecto_DAO;
@@ -19,19 +18,12 @@ public class GestorTrayecto {
 		super();
 		this.TrayectoDAO = new Trayecto_DAO_PostgreSQL();
 	}
-	 //??
+	 
 	public Trayecto crearTrayecto() throws CamposIncorrectosException, SQLException, BaseDeDatosException
 	{
 		Trayecto t = new Trayecto();
-		//this.actualizarModelo(t); ///??
 		return TrayectoDAO.insertarTrayecto(t);
-	} //?
-	
-	
-	/*public void actualizarModelo(Trayecto t)
-	{
-		
-	}*/
+	} 
 	
 	public List<Trayecto> listarTodos() 
 	{
