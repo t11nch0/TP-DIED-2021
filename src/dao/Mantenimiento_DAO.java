a56a695195f8253c5fb4f22abdc02e9285f971a4
JeremiasMVL <71725020+JeremiasMVL@users.noreply.github.com>
2021-08-03 22:28:35 -0300

    update limpieza comentarios

--- src/dao/Mantenimiento_DAO.java
+++ src/dao/Mantenimiento_DAO.java
@@ -8,14 +8,8 @@ import excepciones.BaseDeDatosException;
 public interface Mantenimiento_DAO {
 
 	public List<TareaMantenimiento> buscarTodos();
-	
 	public TareaMantenimiento insertarMantenimiento(TareaMantenimiento mantenimiento) throws BaseDeDatosException, SQLException;
-	
-//	public void eliminarMantenimiento(TareaMantenimiento mantenimiento)throws BaseDeDatosException, SQLException;
-//	public TareaMantenimiento editarMantenimiento(TareaMantenimiento mantenimiento) throws BaseDeDatosException, SQLException;
-	
 	public TareaMantenimiento finalizarMantenimiento(TareaMantenimiento mantenimiento) throws BaseDeDatosException, SQLException;
-
 	
 	public List<TareaMantenimiento> buscarPorIdEstacion(Integer idEstacion); //?
 		
