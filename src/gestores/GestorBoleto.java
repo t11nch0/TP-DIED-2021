9e0e1be3797e086389afd026b4fc8e03919b0e85
JeremiasMVL <71725020+JeremiasMVL@users.noreply.github.com>
2021-08-03 22:48:57 -0300

    update limpieza comentarios

--- src/gestores/GestorBoleto.java
+++ src/gestores/GestorBoleto.java
@@ -2,17 +2,12 @@ package gestores;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
-//import java.util.ArrayList;
 import java.util.List;
 
 import dao.Boleto_DAO;
 import dao.Boleto_DAO_PostgreSQL;
-//import dao.Estacion_DAO;
-//import dao.Estacion_DAO_PostgreSQL;
 import dominio.Boleto;
 import dominio.Camino;
-//import dominio.EstacionDeTransbordoMultimodal;
-//import dominio.EstacionDeTransbordoMultimodal.EstadoEstacion;
 import excepciones.BaseDeDatosException;
 import excepciones.CamposIncorrectosException;
 
@@ -25,25 +20,22 @@ public class GestorBoleto {
 		this.boletoDAO = new Boleto_DAO_PostgreSQL();
 	}
 	public Boleto crearBoleto(Integer nroBoleto, String email, String nombre, LocalDate fecha, Camino camino) throws CamposIncorrectosException, SQLException, BaseDeDatosException
-	{ //??
+	{ 
 		Boleto b = new Boleto();
 		this.actualizarModelo(b, nroBoleto, email, nombre, fecha, camino);
 		return boletoDAO.insertarBoleto(b);
 	}
-	
-	
-	//?
-	public void actualizarModelo(Boleto b, Integer nroBoleto, String email, String nombre, LocalDate fecha, Camino camino) //b y lo demas????
+
+	public void actualizarModelo(Boleto b, Integer nroBoleto, String email, String nombre, LocalDate fecha, Camino camino) 
 	{
-		b.setNroBoleto(nroBoleto); //?
+		b.setNroBoleto(nroBoleto);
 		b.setEmailCliente(email);
 		b.setNombreCliente(nombre);
 		b.setFechaVenta(fecha);
 		b.setCamino(camino);
-		
-		b.setCosto(camino.getCostoTotal()); //?
+		b.setCosto(camino.getCostoTotal());
 	}
-	//
+	
 	public List<Boleto> listarTodos() 
 	{
 		return boletoDAO.buscarTodos();
@@ -53,6 +45,5 @@ public class GestorBoleto {
 	{
 		return boletoDAO.buscarPorId(id);
 	}
-	//
-
+	
 }
