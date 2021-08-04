cced1a5c41a6123c6b17c43266131d3899f0821c
JeremiasMVL <71725020+JeremiasMVL@users.noreply.github.com>
2021-08-03 22:33:35 -0300

    update limpieza comentarios

--- src/dao/Trayecto_DAO.java
+++ src/dao/Trayecto_DAO.java
@@ -3,7 +3,6 @@ package dao;
 import java.sql.SQLException;
 import java.util.List;
 
-//import dominio.LineaTransporte;
 import dominio.Trayecto;
 import excepciones.BaseDeDatosException;
 
@@ -16,7 +15,6 @@ public interface Trayecto_DAO
 	public void eliminarTrayecto();
 	public Trayecto editarTrayecto(Trayecto trayecto) throws BaseDeDatosException, SQLException;
 
-	public Trayecto buscarPorId(Integer id); //?
-	
-	public List<Trayecto> buscarPorIdLinea(Integer id); //?
+	public Trayecto buscarPorId(Integer id); 
+	public List<Trayecto> buscarPorIdLinea(Integer id); 
 }
